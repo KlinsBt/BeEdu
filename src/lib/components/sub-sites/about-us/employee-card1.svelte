c317d2a594df1112c1abcef8bda8272b5106c32b
KlinsBt <klinsbt@gmail.com>
2024-07-16 17:08:00 +0200

    Modify employee card component style

--- src/lib/components/sub-sites/about-us/employee-card1.svelte
+++ src/lib/components/sub-sites/about-us/employee-card1.svelte
@@ -1,26 +1,16 @@
 <script>
-
-export let name = "";
-export let title = "";
-export let description = "";
-export let src = "";
-export let alt = "";
-
+    export let name = "";
+    export let title = "";
+    export let description = "";
+    export let src = "";
+    export let alt = "";
 </script>
-
-
-
-
-
-
-
-
-
 
 <div class="card">
     <div class="circle">
+        <div class="img-border"></div>
         <div class="img-box">
-            <img src={src} alt={alt}>
+            <img {src} {alt} />
         </div>
     </div>
     <div class="content">
@@ -34,117 +24,115 @@ export let alt = "";
     </div>
 </div>
 
-
-
-
-
-
-
-
-
-
 <style>
-
-.card {
-    position: relative;
-    max-width: 320px;
-    min-height: 400px;
-    background: #fff;
-    overflow: hidden;
-    border-radius: 10px;
-    box-shadow: 1px 1px 15px 1px rgb(160, 157, 157);
-}
-
-.circle {
-    position: absolute;
-    top: -190px;
-    left: 50%;
-    width: 500px;
-    height: 500px;
-    transform: translateX(-50%);
-    clip-path: circle();
-}
-
-.circle::before {
-    content: '';
-    position: absolute;
-    top: -8px;
-    left: -16px;
-    width: 100%;
-    height: 100%;
-    box-shadow: 0 0 0 20px rgba(214,177,140,0.7);
-    border-radius: 50%;
-    z-index: 2;
-    pointer-events: none;
-}
-
-.img-box {
-    position: absolute;
-    left: 50%;
-    bottom: 0;
-    transform: translateX(-50%);
-    width: 340px;
-    height: 310px;
-}
-
-img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
-    transition: all 0.5s;
-}
-
-.content {
-    margin-top: 300px;
-    padding: 10px 20px 20px 20px;
-}
-
-.content > h3 {
-    font-size: 1.4em;
-    color: #333;
-    margin-top: 0px;
-    margin-bottom: 2px;
-}
-
-.sub-text {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-
-.sub-text > h4 {
-    color: #d6b18c;
-    font-weight: 500;
-    margin: 0px 0px 0px 0px;
-    /*text-shadow: 1px 1px 15px #7c7c7c73;*/
-}
-
-.text-container {
-    overflow: hidden;
-    padding: 0px 0px 0px 0px;
-}
-
-p {
-    padding: 5px 0px 0px 0px;
-    font-size: 0.8em;
-    font-weight: 400;
-}
-
-@media (min-width: 250px) and (max-width: 340px) {
-
     .card {
-        height: 750px;
+        position: relative;
+        max-width: 320px;
+        min-height: 400px;
+        background: #fff;
+        overflow: hidden;
+        border-radius: 10px;
+        box-shadow: 1px 1px 15px 1px rgb(160, 157, 157);
+    }
+
+    .img-border {
+        position: absolute;
+        top: 190px;
+        left: 50%;
+        width: 280px;
+        height: 310px;
+        transform: translateX(-50%);
+        /* border: 30px solid rgba(214, 177, 140, 0.7); */
+        box-shadow: 0 0 0 20px rgba(214, 177, 140, 0.7);
+        background-color: rgba(214, 177, 140, 0.7);
+    }
+
+    .circle {
+        position: absolute;
+        top: -190px;
+        left: 50%;
+        width: 500px;
+        height: 500px;
+        transform: translateX(-50%);
+        clip-path: circle();
+    }
+
+    .circle::before {
+        content: "";
+        position: absolute;
+        top: -8px;
+        left: -16px;
+        width: 100%;
+        height: 100%;
+        box-shadow: 0 0 0 20px rgba(214, 177, 140, 0.7);
+        border-radius: 50%;
+        z-index: 2;
+        pointer-events: none;
+    }
+
+    .img-box {
+        position: absolute;
+        left: 50%;
+        bottom: 0;
+        transform: translateX(-50%);
+        width: 290px;
+        height: 300px;
+    }
+
+    img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+        transition: all 0.5s;
+        border-radius: 20px;
     }
 
     .content {
-        height: 420px !important;
+        margin-top: 300px;
+        padding: 10px 20px 20px 20px;
     }
-}
 
+    .content > h3 {
+        font-size: 1.4em;
+        color: #333;
+        margin-top: 0px;
+        margin-bottom: 2px;
+    }
 
+    .sub-text {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
 
-    
+    .sub-text > h4 {
+        color: #d6b18c;
+        font-weight: 500;
+        margin: 0px 0px 0px 0px;
+        /*text-shadow: 1px 1px 15px #7c7c7c73;*/
+    }
+
+    .text-container {
+        overflow: hidden;
+        padding: 0px 0px 0px 0px;
+    }
+
+    p {
+        padding: 5px 0px 0px 0px;
+        font-size: 0.8em;
+        font-weight: 400;
+    }
+
+    @media (min-width: 250px) and (max-width: 340px) {
+        .card {
+            height: 750px;
+        }
+
+        .content {
+            height: 420px !important;
+        }
+    }
 </style>
