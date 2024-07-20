f3b31931e58554d314492a1f218c16788880a6ea
KlinsBt <klinsbt@gmail.com>
2024-07-20 04:11:09 +0200

    Update courses

--- src/lib/components/sub-sites/education-travel/course3.svelte
+++ src/lib/components/sub-sites/education-travel/course3.svelte
@@ -1,14 +1,12 @@
 <script>
-
-export let box_lang_title = "";
-export let background_img = "";
-export let p1 = "";
-export let p2 = "";
-export let p3 = "";
-export let p4 = "";
-export let link = "";
-export let button = "";
-
+    export let box_lang_title = "";
+    export let background_img = "";
+    export let p1 = "";
+    export let p2 = "";
+    export let p3 = "";
+    export let p4 = "";
+    export let link = "";
+    export let button = "";
 </script>
 
 <div class="container">
@@ -24,7 +22,7 @@ export let button = "";
                 <li>{p2}</li>
                 <li>{p3}</li>
                 <li>{p4}</li>
-                    <!-- <li>
+                <!-- <li>
                         <div class="time">
                             <div>
                                 <p>
@@ -71,7 +69,7 @@ export let button = "";
                 </li> -->
                 <li>
                     <button class="button" style="vertical-align: middle">
-                        <a href="{link}">
+                        <a href={link}>
                             <span>{button}</span>
                         </a>
                     </button>
@@ -82,49 +80,50 @@ export let button = "";
 </div>
 
 <style>
+    .container {
+        max-width: 300px;
+    }
 
-.container {
-    max-width: 300px;
-}
+    .infos {
+        text-align: center;
+        border: 2px solid #5858585d;
+        background-color: white;
+        color: #777;
+        font-size: 14px;
+        padding-left: 0;
+        margin: 10px 20px;
+        box-shadow:
+            rgba(0, 0, 0, 0.3) 0px 19px 38px,
+            rgba(0, 0, 0, 0.22) 0px 15px 12px;
+        border-radius: 15px;
+    }
 
-.infos {
-    text-align: center;
-    border: 2px solid #5858585d;
-    background-color: white;
-    color: #777;
-    font-size: 14px;
-    padding-left: 0;
-    margin: 10px 20px;
-    box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
-    border-radius: 15px;
-}
+    .bgr {
+        background: var(--image);
+        /*background: linear-gradient(100deg, rgba(5,6,8,1) 0%, rgba(222,31,38,0.8547794117647058) 55%, rgba(252,206,8,1) 100%);*/
+        background-size: 370px 170px;
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-blend-mode: overlay;
+        border-radius: 15px 15px 0px 0px;
+        background-color: rgba(255, 255, 255, 0.15);
+    }
 
-.bgr {
-    background: var(--image);
-    /*background: linear-gradient(100deg, rgba(5,6,8,1) 0%, rgba(222,31,38,0.8547794117647058) 55%, rgba(252,206,8,1) 100%);*/
-    background-size: 370px 170px;
-    background-repeat:no-repeat;
-    background-position:center center;
-    background-blend-mode: overlay;
-    border-radius: 15px 15px 0px 0px;
-    background-color: rgba(255, 255, 255, 0.15);
-}
+    .bgr > h3 {
+        font-size: 32px;
+        font-weight: 700;
+        text-shadow: 2px 2px 15px rgb(0, 0, 0);
+        color: rgb(255, 255, 255);
+        -webkit-text-stroke-width: 2px;
+        -webkit-text-stroke-color: rgb(0, 0, 0);
+    }
 
-.bgr > h3 {
-    font-size: 32px;
-    font-weight: 700;
-    text-shadow: 2px 2px 15px rgb(0, 0, 0);
-    color:rgb(255, 255, 255);
-    -webkit-text-stroke-width: 2px;
-    -webkit-text-stroke-color: rgb(0, 0, 0);
-}
+    .infos li:first-child,
+    .infos li:last-child {
+        padding: 20px 13px;
+    }
 
-.infos li:first-child,
-.infos li:last-child {
-    padding: 20px 13px;
-}
-
-/* .time {
+    /* .time {
     display: grid;
     grid-template-columns: auto auto auto;
 }
@@ -133,114 +132,111 @@ export let button = "";
     padding-bottom: 5px !important;
 } */
 
-.infos > li {
-    list-style: none;
-    padding: 10px;
-}
-
-.infos > li + li {
-    border-top: 1px solid #f0f0f0;
-}
-
-span {
-    font-size: 10px;
-    color: #999;
-    font-weight: 300;
-}
-
-.infos > li:nth-last-child(2) {
-    padding: 10px;
-}
-
-.button {
-    display: inline-block;
-    border-radius: 4px;
-    /*background-color: #f4511e;*/
-    background-color: #d6b18c;
-    border: none;
-    color: #FFFFFF;
-    text-align: center;
-    padding: 5px;
-    width: 170px;
-    height: 50px;
-    transition: all 0.5s;
-    cursor: pointer;
-    margin: 5px;
-}
-
-.button span {
-    cursor: pointer;
-    display: inline-block;
-    position: relative;
-    transition: 0.5s;
-}
-
-.button span:after {
-    content: '\00bb';
-    position: absolute;
-    opacity: 0;
-    top: 0;
-    right: -30px;
-    transition: 0.4s;
-}
-
-.button:hover span {
-    padding-right: 20px;
-}
-
-.button:hover span:after {
-    opacity: 1;
-    right: 0;
-}
-
-.button > a > span {
-    font-size: 24px;
-    font-weight: 500;
-    color: #FFFFFF;
-}
-
-h3 {
-    font-size: 28px;
-    font-weight: 500;
-}
-
-li {
-    font-size: 14px;
-}
-
-p {
-    font-size: 14px;
-}
-
-li > h3 {
-    padding: 0px;
-    margin: 0px;
-}
-
-@media (min-width: 50px) and (max-width: 280px) {
-
-    .infos {
-        width: 200px;
-        margin: 10px 10px;
+    .infos > li {
+        list-style: none;
+        padding: 10px;
     }
 
-    .bgr > h3 {
-        font-size: 30px;
-        font-weight: 700;
+    .infos > li + li {
+        border-top: 1px solid #f0f0f0;
+    }
+
+    span {
+        font-size: 10px;
+        color: #999;
+        font-weight: 300;
+    }
+
+    .infos > li:nth-last-child(2) {
+        padding: 10px;
+    }
+
+    .button {
+        display: inline-block;
+        border-radius: 4px;
+        /*background-color: #f4511e;*/
+        background-color: #d6b18c;
+        border: none;
+        color: #ffffff;
+        text-align: center;
+        padding: 5px;
+        width: 170px;
+        height: 50px;
+        transition: all 0.5s;
+        cursor: pointer;
+        margin: 5px;
+    }
+
+    .button span {
+        cursor: pointer;
+        display: inline-block;
+        position: relative;
+        transition: 0.5s;
+    }
+
+    .button span:after {
+        content: "\00bb";
+        position: absolute;
+        opacity: 0;
+        top: 0;
+        right: -30px;
+        transition: 0.4s;
+    }
+
+    .button:hover span {
+        padding-right: 20px;
+    }
+
+    .button:hover span:after {
+        opacity: 1;
+        right: 0;
+    }
+
+    .button > a > span {
+        font-size: 24px;
+        font-weight: 500;
+        color: #ffffff;
     }
 
     h3 {
-        font-size: 17px;
-    }
-
-    p {
-        font-size: 12px;
+        font-size: 28px;
+        font-weight: 500;
     }
 
     li {
-    font-size: 12px;
+        font-size: 14px;
     }
 
-}
+    p {
+        font-size: 14px;
+    }
 
+    li > h3 {
+        padding: 0px;
+        margin: 0px;
+    }
+
+    @media (min-width: 50px) and (max-width: 280px) {
+        .infos {
+            width: 200px;
+            margin: 10px 10px;
+        }
+
+        .bgr > h3 {
+            font-size: 30px;
+            font-weight: 700;
+        }
+
+        h3 {
+            font-size: 17px;
+        }
+
+        p {
+            font-size: 12px;
+        }
+
+        li {
+            font-size: 12px;
+        }
+    }
 </style>
--- src/routes/(main)/courses/+page.svelte
+++ src/routes/(main)/courses/+page.svelte
@@ -1,17 +1,6 @@
 <script>
-	
-import Course from "$lib/components/sub-sites/education-travel/course3.svelte";
-    
+    import Course from "$lib/components/sub-sites/education-travel/course3.svelte";
 </script>
-
-
-
-
-
-
-
-
-
 
 <div class="main-container">
     <div class="page-title-container">
@@ -19,8 +8,8 @@ import Course from "$lib/components/sub-sites/education-travel/course3.svelte";
         <h3>Here you can see our Berndt Education course catalogue.</h3>
     </div>
     <div class="boxes">
-        <Course 
-            box_lang_title="BE German" 
+        <Course
+            box_lang_title="BE German"
             background_img="/img/flags/ger.png"
             p1="Learn German from home via Zoom with qualified
             teachers and an engaging learning system."
@@ -28,10 +17,10 @@ import Course from "$lib/components/sub-sites/education-travel/course3.svelte";
             p3="4 times a week"
             p4="From Monday to Thursday"
             link="/contact"
-            button="Join Now" 
+            button="Join Now"
         />
-        <Course 
-            box_lang_title="BE Spanish" 
+        <Course
+            box_lang_title="BE Spanish"
             background_img="/img/flags/esp.png"
             p1="Learn Spanish from home via Zoom with qualified
             teachers and an engaging learning system."
@@ -39,10 +28,10 @@ import Course from "$lib/components/sub-sites/education-travel/course3.svelte";
             p3="4 times a week"
             p4="From Monday to Thursday"
             link="/contact"
-            button="Join Now" 
+            button="Join Now"
         />
-        <Course 
-            box_lang_title="BE English" 
+        <Course
+            box_lang_title="BE English"
             background_img="/img/flags/uk.png"
             p1="Learn English from home via Zoom with qualified
             teachers and an engaging learning system."
@@ -50,10 +39,10 @@ import Course from "$lib/components/sub-sites/education-travel/course3.svelte";
             p3="4 times a week"
             p4="From Monday to Thursday"
             link="/contact"
-            button="Join Now" 
+            button="Join Now"
         />
-        <Course 
-            box_lang_title="BE French" 
+        <Course
+            box_lang_title="BE French"
             background_img="/img/flags/frn.png"
             p1="Learn French from home via Zoom with qualified
             teachers and an engaging learning system."
@@ -61,41 +50,75 @@ import Course from "$lib/components/sub-sites/education-travel/course3.svelte";
             p3="4 times a week"
             p4="From Monday to Thursday"
             link="/contact"
-            button="Join Now" 
+            button="Join Now"
+        />
+    </div>
+
+    <div class="boxes-2">
+        <Course
+            box_lang_title="Learn Calculus"
+            background_img="/img/bg2.jpg"
+            p1="Learn Calculus from home via Zoom with qualified
+            teachers and an engaging learning system."
+            p2="Individual or small groups"
+            p3="Flexible schedule"
+            p4="Flexible routine"
+            link="/contact"
+            button="Join Now"
+        />
+
+        <Course
+            box_lang_title="Learn Math"
+            background_img="/img/bg2.jpg"
+            p1="Learn Math from home via Zoom with qualified
+            teachers and an engaging learning system."
+            p2="Individual or small groups"
+            p3="Flexible schedule"
+            p4="Flexible routine"
+            link="/contact"
+            button="Join Now"
+        />
+
+        <Course
+            box_lang_title="Learn Physics"
+            background_img="/img/bg2.jpg"
+            p1="Learn Physics from home via Zoom with qualified
+            teachers and an engaging learning system."
+            p2="Individual or small groups"
+            p3="Flexible schedule"
+            p4="Flexible routine"
+            link="/contact"
+            button="Join Now"
         />
     </div>
 </div>
 
-    
-
-
-
-
-
-
-
-
 <style>
+    .main-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: start;
+        align-items: center;
+        min-height: 100vh;
+    }
 
-.main-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: start;
-    align-items: center;
-    min-height: 100vh;
-}
+    .page-title-container {
+        text-align: center;
+        margin: 50px 0px;
+        padding: 0px 15px;
+    }
 
-.page-title-container {
-    text-align: center;
-    margin: 50px 0px;
-    padding: 0px 15px;
-}
+    .boxes {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        margin-bottom: 50px;
+    }
 
-.boxes {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    margin-bottom: 50px;
-}
-
+    .boxes-2 {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        margin-bottom: 50px;
+    }
 </style>
